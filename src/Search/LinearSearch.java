133596566a0ab69418396d3d6842cd9817839c67
Yshika <yshika0581@gmail.com>
2019-02-21 13:55:27 +0530

    Initial commit

---
+++ src/Search/LinearSearch.java
@@ -0,0 +1,63 @@
+/*
+package Search;
+
+import java.util.Arrays;
+
+public class LinearSearch {
+    */
+/*public static void main(String[] args) {
+        int[] num=new int[10];
+        Display(num);
+        int pos=-1;
+        pos=linear(num,10);
+    }
+
+    public static void Display(int[] num) {
+        for (int i = 0; i < num.length; i++) {
+            num[i]=(int)(Math.random()*10);
+        }
+        System.out.println(Arrays.toString(num));
+    }
+    public static void linear(int[] num,int n){
+        int pos=-1;
+        for (int i = 0; i < num.length; i++) {
+            if
+        }
+    }*//*
+
+
+
+    public static void main(String[] args) {
+        int size=10;
+        Car[] car=new Car[size];
+        for (int i = 0; i < car.length ; i++) {
+            car[i]=new Car("Company "+i,i*10);
+
+        }
+    }
+}
+class Car{
+    private String company;
+    private int price;
+
+    public Car(String company, int price) {
+        this.company = company;
+        this.price = price;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}*/
